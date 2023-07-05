1ec8443d6890f5077a61df5dd814b64267a67b6c
Junil Jacob <juniljacob124@gmail.com>
2023-07-05 22:01:41 +0530

    Fix cors issue

--- bin/server.js
+++ bin/server.js
@@ -8,7 +8,7 @@ const http = require('http')
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://domain1.compliancemanager.lvh.me:3002',
     methods: ['GET', 'POST']
   }
 })
