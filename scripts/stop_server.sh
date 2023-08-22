313aa6bcc0b8ca8b2824af8401bc0c16ae112ac7
TONY RIVERA <trivera@rmsoa.cpm>
2023-08-22 19:11:19 -0400

    updated ymls

---
+++ scripts/stop_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Stopping all PM2 managed applications..."
+pm2 stop all
+if [ $? -eq 0 ]
+then
+  echo "Successfully stopped all PM2 managed applications."
+else
+  echo "Failed to stop all PM2 managed applications, trying to forcefully kill node processes..."
+  pkill -9 node
+  if [ $? -eq 0 ]
+  then
+    echo "Successfully killed node processes."
+  else
+    echo "Failed to kill node processes. Manual intervention might be required."
+  fi
+fi
