a6404ed0e0797f4454bd4e51dcce08807212f821
Tony Rivera <85594926+rTonyCloud@users.noreply.github.com>
2023-09-28 23:18:13 -0400

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-LOGS_DIR="/home/centos/Doc-Project-API/"
+# Define log and directory paths
+LOGS_DIR="/home/centos/Doc-Project-API/logs"
+LOG_FILE="/home/centos/Doc-Project-API/deletion.log"
+
+# Function to log messages with timestamps
+log() {
+  local message="$1"
+  local timestamp="$(date +'%Y-%m-%d %H:%M:%S')"
+  echo "$timestamp - $message" >> "$LOG_FILE"
+}
+
+# Function to delete log files
+delete_log_files() {
+  local deleted_files=("$LOGS_DIR"/*)
+  rm -rf "$LOGS_DIR"/*
+  return $?
+}
 
 # Check if the logs directory exists
 if [ -d "$LOGS_DIR" ]; then
-  # Remove all log files in the directory
-  rm -rf "$LOGS_DIR"/*
-  echo "Deleted all log files in: $LOGS_DIR"
+  delete_log_files
+  if [ $? -eq 0 ]; then
+    log "Deleted ${#deleted_files[@]} log files in: $LOGS_DIR"
+  else
+    log "Failed to delete log files in: $LOGS_DIR"
+  fi
 else
-  echo "Logs directory does not exist: $LOGS_DIR"
+  log "Logs directory does not exist: $LOGS_DIR"
 fi
