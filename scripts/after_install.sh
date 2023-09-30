8e762b07464b5c2fc0a167934b9bae22f4a82eb9
Tony Rivera <85594926+rTonyCloud@users.noreply.github.com>
2023-09-30 02:44:53 -0400

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,56 +2,44 @@
 
 # Set the AWS CLI region explicitly
 aws configure set region us-west-2
-export AWS_DEFAULT_REGION=us-west-2
 
-# Retrieve the environment variables from AWS SSM
-echo "Retrieving environment variables from AWS SSM..."
+# Define an array of parameter names
+PARAMETER_NAMES=(
+  "/Doc-Project-API/NODE_ENV"
+  "/Doc-Project-API/MONGODB_NAME"
+  "/Doc-Project-API/LOCAL_MONGODB_URL"
+  "/Doc-Project-API/LOCAL_MONGODB_USER"
+  "/Doc-Project-API/LOCAL_MONGODB_PASS"
+  "/Doc-Project-API/LOCAL_API_URL"
+  "/Doc-Project-API/S3_BUCKET"
+  "/Doc-Project-API/ID"
+  "/Doc-Project-API/SECRET"
+  "/Doc-Project-API/USER_ID"
+  "/Doc-Project-API/API_ACCOUNT_ID"
+  "/Doc-Project-API/ACCOUNT_BASE_URI"
+  "/Doc-Project-API/INTEGRATION_KEY"
+  "/Doc-Project-API/CORS_ORIGIN"
+  "/Doc-Project-API/JWT_SECRET"
+  "/Doc-Project-API/PLATFORM_DB_NAME"
+  "/Doc-Project-API/PLATFORM_USERNAME"
+  "/Doc-Project-API/PLATFORM_DB_HOST"
+  "/Doc-Project-API/PLATFORM_DB_DIALECT"
+  "/Doc-Project-API/PLATFORM_PASSWORD"
+)
 
-NODE_ENV=$(aws ssm get-parameter --name "/Doc-Project-API/NODE_ENV" --query "Parameter.Value" --output text)
-MONGODB_NAME=$(aws ssm get-parameter --name "/Doc-Project-API/MONGODB_NAME" --query "Parameter.Value" --output text)
-LOCAL_MONGODB_URL=$(aws ssm get-parameter --name "/Doc-Project-API/LOCAL_MONGODB_URL" --query "Parameter.Value" --output text)
-LOCAL_MONGODB_USER=$(aws ssm get-parameter --name "/Doc-Project-API/LOCAL_MONGODB_USER" --query "Parameter.Value" --output text)
-LOCAL_MONGODB_PASS=$(aws ssm get-parameter --name "/Doc-Project-API/LOCAL_MONGODB_PASS" --query "Parameter.Value" --output text)
-LOCAL_API_URL=$(aws ssm get-parameter --name "/Doc-Project-API/LOCAL_API_URL" --query "Parameter.Value" --output text)
-S3_BUCKET=$(aws ssm get-parameter --name "/Doc-Project-API/S3_BUCKET" --query "Parameter.Value" --output text)
-ID=$(aws ssm get-parameter --name "/Doc-Project-API/ID" --query "Parameter.Value" --output text)
-SECRET=$(aws ssm get-parameter --name "/Doc-Project-API/SECRET" --query "Parameter.Value" --output text)
-USER_ID=$(aws ssm get-parameter --name "/Doc-Project-API/USER_ID" --query "Parameter.Value" --output text)
-API_ACCOUNT_ID=$(aws ssm get-parameter --name "/Doc-Project-API/API_ACCOUNT_ID" --query "Parameter.Value" --output text)
-ACCOUNT_BASE_URI=$(aws ssm get-parameter --name "/Doc-Project-API/ACCOUNT_BASE_URI" --query "Parameter.Value" --output text)
-INTEGRATION_KEY=$(aws ssm get-parameter --name "/Doc-Project-API/INTEGRATION_KEY" --query "Parameter.Value" --output text)
-CORS_ORIGIN=$(aws ssm get-parameter --name "/Doc-Project-API/CORS_ORIGIN" --query "Parameter.Value" --output text)
-JWT_SECRET=$(aws ssm get-parameter --name "/Doc-Project-API/JWT_SECRET" --query "Parameter.Value" --output text)
-PLATFORM_DB_NAME=$(aws ssm get-parameter --name "/Doc-Project-API/PLATFORM_DB_NAME" --query "Parameter.Value" --output text)
-PLATFORM_USERNAME=$(aws ssm get-parameter --name "/Doc-Project-API/PLATFORM_USERNAME" --query "Parameter.Value" --output text)
-PLATFORM_DB_HOST=$(aws ssm get-parameter --name "/Doc-Project-API/PLATFORM_DB_HOST" --query "Parameter.Value" --output text)
-PLATFORM_DB_DIALECT=$(aws ssm get-parameter --name "/Doc-Project-API/PLATFORM_DB_DIALECT" --query "Parameter.Value" --output text)
-PLATFORM_PASSWORD=$(aws ssm get-parameter --name "/Doc-Project-API/PLATFORM_PASSWORD" --query "Parameter.Value" --output text)
+# Create or truncate the .env file
+> .env
 
-# Store the environment variables in a .env file
-echo "Creating .env file..."
-
-cat <<EOF > .env
-NODE_ENV=${NODE_ENV}
-MONGODB_NAME=${MONGODB_NAME}
-LOCAL_MONGODB_URL=${LOCAL_MONGODB_URL}
-LOCAL_MONGODB_USER=${LOCAL_MONGODB_USER}
-LOCAL_MONGODB_PASS=${LOCAL_MONGODB_PASS}
-LOCAL_API_URL=${LOCAL_API_URL}
-S3_BUCKET=${S3_BUCKET}
-ID=${ID}
-SECRET=${SECRET}
-USER_ID=${USER_ID}
-API_ACCOUNT_ID=${API_ACCOUNT_ID}
-ACCOUNT_BASE_URI=${ACCOUNT_BASE_URI}
-INTEGRATION_KEY=${INTEGRATION_KEY}
-CORS_ORIGIN=${CORS_ORIGIN}
-JWT_SECRET=${JWT_SECRET}
-PLATFORM_DB_NAME=${PLATFORM_DB_NAME}
-PLATFORM_USERNAME=${PLATFORM_USERNAME}
-PLATFORM_DB_HOST=${PLATFORM_DB_HOST}
-PLATFORM_DB_DIALECT=${PLATFORM_DB_DIALECT}
-PLATFORM_PASSWORD=${PLATFORM_PASSWORD}
-EOF
+# Retrieve and set the environment variables from AWS SSM
+for PARAMETER_NAME in "${PARAMETER_NAMES[@]}"; do
+  PARAMETER_VALUE=$(aws ssm get-parameter --name "$PARAMETER_NAME" --query "Parameter.Value" --output text 2>/dev/null)
+  
+  if [ -n "$PARAMETER_VALUE" ]; then
+    echo "$PARAMETER_NAME=$PARAMETER_VALUE" >> .env
+    echo "Retrieved $PARAMETER_NAME"
+  else
+    echo "Warning: $PARAMETER_NAME not found or empty."
+  fi
+done
 
 echo ".env file created successfully."
