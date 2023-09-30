514134dd254563b8a30a51ce18ca7deb4ecaa22b
Tony Rivera <85594926+rTonyCloud@users.noreply.github.com>
2023-09-30 03:12:11 -0400

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,28 +3,28 @@
 # Set the AWS CLI region explicitly
 aws configure set region us-west-2
 
-# Define an array of parameter names
+# Define an array of parameter names without the prefix
 PARAMETER_NAMES=(
-  "/Doc-Project-API/NODE_ENV"
-  "/Doc-Project-API/MONGODB_NAME"
-  "/Doc-Project-API/LOCAL_MONGODB_URL"
-  "/Doc-Project-API/LOCAL_MONGODB_USER"
-  "/Doc-Project-API/LOCAL_MONGODB_PASS"
-  "/Doc-Project-API/LOCAL_API_URL"
-  "/Doc-Project-API/S3_BUCKET"
-  "/Doc-Project-API/ID"
-  "/Doc-Project-API/SECRET"
-  "/Doc-Project-API/USER_ID"
-  "/Doc-Project-API/API_ACCOUNT_ID"
-  "/Doc-Project-API/ACCOUNT_BASE_URI"
-  "/Doc-Project-API/INTEGRATION_KEY"
-  "/Doc-Project-API/CORS_ORIGIN"
-  "/Doc-Project-API/JWT_SECRET"
-  "/Doc-Project-API/PLATFORM_DB_NAME"
-  "/Doc-Project-API/PLATFORM_USERNAME"
-  "/Doc-Project-API/PLATFORM_DB_HOST"
-  "/Doc-Project-API/PLATFORM_DB_DIALECT"
-  "/Doc-Project-API/PLATFORM_PASSWORD"
+  "NODE_ENV"
+  "MONGODB_NAME"
+  "LOCAL_MONGODB_URL"
+  "LOCAL_MONGODB_USER"
+  "LOCAL_MONGODB_PASS"
+  "LOCAL_API_URL"
+  "S3_BUCKET"
+  "ID"
+  "SECRET"
+  "USER_ID"
+  "API_ACCOUNT_ID"
+  "ACCOUNT_BASE_URI"
+  "INTEGRATION_KEY"
+  "CORS_ORIGIN"
+  "JWT_SECRET"
+  "PLATFORM_DB_NAME"
+  "PLATFORM_USERNAME"
+  "PLATFORM_DB_HOST"
+  "PLATFORM_DB_DIALECT"
+  "PLATFORM_PASSWORD"
 )
 
 # Create or truncate the .env file
@@ -32,13 +32,14 @@ PARAMETER_NAMES=(
 
 # Retrieve and set the environment variables from AWS SSM
 for PARAMETER_NAME in "${PARAMETER_NAMES[@]}"; do
-  PARAMETER_VALUE=$(aws ssm get-parameter --name "$PARAMETER_NAME" --query "Parameter.Value" --output text 2>/dev/null)
+  FULL_PARAMETER_NAME="/Doc-Project-API/$PARAMETER_NAME"
+  PARAMETER_VALUE=$(aws ssm get-parameter --name "$FULL_PARAMETER_NAME" --query "Parameter.Value" --output text 2>/dev/null)
   
   if [ -n "$PARAMETER_VALUE" ]; then
     echo "$PARAMETER_NAME=$PARAMETER_VALUE" >> .env
-    echo "Retrieved $PARAMETER_NAME"
+    echo "Retrieved $FULL_PARAMETER_NAME"
   else
-    echo "Warning: $PARAMETER_NAME not found or empty."
+    echo "Warning: $FULL_PARAMETER_NAME not found or empty."
   fi
 done
 
