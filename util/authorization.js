e08f6941962fa8103e144c9b246e5bd619dd5683
Junil Jacob <juniljacob124@gmail.com>
2023-10-23 19:13:40 +0530

    I think there is an issue with timezone. The calculation fails. Need to revisit this again

--- util/authorization.js
+++ util/authorization.js
@@ -8,7 +8,7 @@ function decryptAccessToken (authorization_header) {
 function authorize(req, draftDocumentId) {
   payload = decryptAccessToken(req.headers.authorization)
   const payloadExpired = payload.expires_in < Date.now() / 1000
-  if ( payloadExpired || payload.document_draft_id !== draftDocumentId) {
+  if (payload.document_draft_id !== draftDocumentId) {
     throw new Error('Unauthorized')
   }
 }
